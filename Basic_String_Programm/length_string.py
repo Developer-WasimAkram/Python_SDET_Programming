1435b6792f1c229c8c95476da28f39cf09af2378
Developer-WasimAkram <noorewasim@gmail.com>
2024-12-14 01:26:58 +0530

    length_string

--- Basic_String_Programm/length_string.py
+++ Basic_String_Programm/length_string.py
@@ -6,3 +6,13 @@ def length_string(string):
     return len(string)
 s='Hello World'
 print("Method_1: Legth of string is ",length_string(s))
+
+#Method 2 using count
+
+def string_length_count(string):
+    count=0 
+    for s in string:
+        count+=1
+    return count
+
+print("Method_w: Legth of string using count  is ",string_length_count(s))
