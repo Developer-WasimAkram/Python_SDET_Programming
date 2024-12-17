ba23e4e0e3f425fc0011b65733809fcbd9ef300d
Developer-WasimAkram <noorewasim@gmail.com>
2024-12-17 22:32:19 +0530

    First-Commit

---
+++ Basic_String_Programm/maxFreqCount.py
@@ -0,0 +1,28 @@
+#Python – Max Frequent Character in String
+
+
+#Metho 1
+
+def leastFrequncyChar(string):
+    char_count={}
+    for char in string:
+        if char in char_count:
+           char_count[char]+=1
+        else:
+           char_count[char]=1
+           
+    return max(char_count,key=char_count.get)
+    
+string='abbcccddddeeeee'  
+print(leastFrequncyChar(string))
+
+
+#Method 2  Using Counter
+
+from collections import Counter
+
+def leastFrequncyChar_Counter(string):
+    char_count = Counter(string)
+    return char_count.most_common()[0][0]
+
+print(leastFrequncyChar_Counter(string))
