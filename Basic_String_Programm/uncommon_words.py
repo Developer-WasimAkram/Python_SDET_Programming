49a0da0429fe246e5bc9a349a21fcfb6f7368194
Developer-WasimAkram <noorewasim@gmail.com>
2024-12-18 12:33:00 +0530

    First commit

---
+++ Basic_String_Programm/uncommon_words.py
@@ -0,0 +1,32 @@
+#Python program to find uncommon words from two Strings
+
+'''
+Given two sentences as strings A and B. The task is to return a list of all uncommon words. A word is uncommon if it appears exactly once in any one of the sentences, and does not appear in the other sentence. Note: A sentence is a string of space-separated words. Each word consists only of lowercase letters. 
+
+Examples:
+Input : A = “Geeks for Geeks”,  B = “Learning from Geeks for Geeks”
+Output : [‘Learning’, ‘from’]
+Input : A = “apple banana mango” , B = “banana fruits mango”
+Output : [‘apple’, ‘fruits’]
+'''
+
+
+def uncommon_words(str1,str2):
+    str1 = str1.split()
+    str2 = str2.split()
+    
+    dict={}
+    dict2={}
+    
+    for word in str1:
+        dict[word] = dict.get(word,0)+1
+    for word in str2:
+        dict[word] = dict.get(word,0)+1
+        
+    return [ word  for word in dict if dict[word]==1]
+    
+ 
+ 
+A = "Geeks for Geeks"
+B = "Learning from Geeks for Geeks"   
+print(uncommon_words(A,B))
